69a9ad90645036999b29f0ff43cec17ce0cc2e18
usmanasgh <usmanasgh@live.com>
2022-02-17 23:46:24 +0000

    Blazor web assembly

---
+++ BlazorWebAssembly.NetCore5/Client/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.NetCore5.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor(); // MUA: Addedd reference dependancies.
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
