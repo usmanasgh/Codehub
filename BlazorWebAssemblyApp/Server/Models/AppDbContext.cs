f5a72f81ecf23559b7bd3fa6a3be8be6f40d59d4
usmanasgh <usmanasgh@live.com>
2022-03-13 17:17:49 +0000

    Mega Changes & restructuring.

---
+++ BlazorWebAssemblyApp/Server/Models/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorWebAssemblyApp.Shared.Business;
+using BlazorWebAssemblyApp.Shared.Common;
+
+namespace BlazorWebAssemblyApp.Server.Models
+{
+    // MUA : Manually created.
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = new Guid(),
+                FirstName = "Usman",
+                LastName = "Asghar",
+                Username = "Admin",
+                Role = new Role { Id = Enums.Role.Admin, Name = "Administrator", ShortName = "Admin" },
+                Phone = "Test",
+                Email = "usmanasgh@gmail.com"
+            });
+        }
+    }
+}
---
+++ BlazorWebAssemblyApp/Shared/Business/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorWebAssemblyApp.Shared.Common;
+
+namespace BlazorWebAssemblyApp.Shared.Business
+{
+    public class Role
+    {
+        public Enums.Role Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+    }
+}
