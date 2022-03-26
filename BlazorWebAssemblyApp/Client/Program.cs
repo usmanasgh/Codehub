c32e2d039866fcfa88e2d2573b3b01e4e438490d
usmanasgh <usmanasgh@live.com>
2022-03-26 16:37:33 +0000

    Adding user service

--- BlazorWebAssemblyApp/Client/Program.cs
+++ BlazorWebAssemblyApp/Client/Program.cs
@@ -8,6 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Syncfusion.Blazor;
+using BlazorWebAssemblyApp.Client.Services;
 
 namespace BlazorWebAssemblyApp.Client
 {
@@ -19,6 +20,12 @@ namespace BlazorWebAssemblyApp.Client
 
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+
+            //MUA : Below service for AddHttpClient
+            builder.Services.AddHttpClient<IUserService, UserService>(user =>
+            {
+                user.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+            });
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ BlazorWebAssemblyApp/Client/Services/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using BlazorWebAssemblyApp.Shared.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssemblyApp.Client.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> Search(string username, string email);
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(Guid userId);
+        Task<User> GetUserByUsername(string username);
+        Task<User> AddUser(User user);
+        Task<User> UpdateUser(User user);
+        Task DeleteUser(Guid userId);
+    }
+}
