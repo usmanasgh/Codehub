ce98f7dffaef529083d29e614bec8d1a6182e335
usmanasgh <usmanasgh@live.com>
2022-07-14 00:29:53 +0100

    Adding MVC

---
+++ CodeHub.NetCore5/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeHub.NetCore5.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "This is string returned by controller method index";
+        }
+    }
+}
--- CodeHub.NetCore5/Startup.cs
+++ CodeHub.NetCore5/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,38 +16,94 @@ namespace CodeHub.NetCore5
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.configuration = configuration;
+            var MUATEST = this.configuration["usmanasgh"];
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+
+            //services.AddMvc(); // MUA: Adding this service to run MVC 1.1
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // MUA : Does this service works as a middleware ?
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //MUA: 1nd middleware code? - 1.0
+            if (env.IsDevelopment() || env.IsEnvironment("MUACustom"))
             {
-                app.UseDeveloperExceptionPage();
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
+                {
+                    SourceCodeLineCount = 10 // MUA: Number of lines to display before and after the lines causes the exception
+                };
+                
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
             }
             else
             {
                 app.UseExceptionHandler("/Error");
             }
 
-            app.UseStaticFiles();
+            //MUA: 2nd middleware code? - 1.1
+            //app.Use(async (context, next) =>
+            //{
+            //    //await context.Response.WriteAsync("First middleware");
+            //    //logger.LogInformation("MW1: Incoming request");  // MW1 = MiddleWare 1
+            //    await next();
+            //    //logger.LogInformation("MW1: Outgoing request"); 
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    //await context.Response.WriteAsync("First middleware");
+            //    logger.LogInformation("MW2: Incoming request");  // MW2 = MiddleWare 2
+            //    await next();
+            //    logger.LogInformation("MW2: Outgoing request");
+            //});
+
+            // - 1.1
+
+
+            //FileServerOptions fileServerOptions = new FileServerOptions();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            //fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("codehub.html");
+
+            //app.UseFileServer(fileServerOptions);
+
+            //app.UseFileServer(); // Will call default.html
+
+
+
+            
+            //app.UseHttpsRedirection();
+
+            app.UseStaticFiles(); // MUA : Adding new middleware request processing pipeline.
 
             app.UseRouting();
 
             app.UseAuthorization();
 
+            //app.Run(async (context) =>
+            //{
+            //    //throw new Exception("Custom exception");
+            //    await context.Response.WriteAsync("Hosting environment : " + env.EnvironmentName);
+
+            //});
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
