387dabe1b94def72f6b6de5f7f9a825b35a49032
usmanasgh <usmanasgh@live.com>
2022-10-01 12:17:03 +0100

    Deleting a role.

--- CodeHub.NetCore5/Controllers/AdministrationController.cs
+++ CodeHub.NetCore5/Controllers/AdministrationController.cs
@@ -306,5 +306,33 @@ namespace CodeHub.NetCore5.Controllers
                 return View("ListUsers");
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await roleManager.DeleteAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View("ListRoles");
+            }
+        }
     }
 }
--- CodeHub.NetCore5/wwwroot/js/codehubcore.js
+++ CodeHub.NetCore5/wwwroot/js/codehubcore.js
@@ -11,3 +11,19 @@
     }
 }
 
+
+function confirmDelete(uniqueId, isTrue) {
+
+    var deleteSpan = 'deleteSpan_' + uniqueId;
+    var confirmDeleteSpan = 'confirmDeleteSpan_' + uniqueId;
+
+    if (isTrue) {
+        $('#' + deleteSpan).hide();
+        $('#' + confirmDeleteSpan).show();
+    } else {
+        $('#' + deleteSpan).show();
+        $('#' + confirmDeleteSpan).hide();
+    }
+}
+
+
