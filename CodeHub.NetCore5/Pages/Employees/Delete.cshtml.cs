fa2e774d7c10f245a7cdd4187fdf00c0c77eb3cf
usmanasgh <usmanasgh@live.com>
2022-10-21 16:38:47 +0100

    Delete employees

---
+++ CodeHub.NetCore5/Pages/Employees/Delete.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeHub.NetCore5.Interface;
+using CodeHub.NetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CodeHub.NetCore5.Pages.Employees
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public DeleteModel(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            Employee = employeeRepository.GetEmployee(id);
+
+            if (Employee == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Employee deletedEmployee = employeeRepository.Delete(Employee.Id);
+
+            if (deletedEmployee == null)
+            {
+                return RedirectToPage("/NotFound");
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+}
