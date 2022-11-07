bc0eb1943c19d9d03d55dfd8a875f1db6033f402
usmanasgh <usmanasgh@live.com>
2022-11-07 00:56:44 +0000

    Adding EDMX without erros (VS2022)

---
+++ CodeHub/Controllers/BusinessEmployeeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CodeHub.Controllers
+{
+    public class BusinessEmployeeController : ApiController
+    {
+        //public IEnumerable<Employee> Get()
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        return entities.Employees.ToList();
+        //    }
+        //}
+
+        //public Employee Get(int id)
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        return entities.Employees.FirstOrDefault(e => e.ID == id);
+        //    }
+        //}
+    }
+}
