c6466e1540aa7fb03191eb315fd07a99216c106e
usmanasgh <usmanasgh@live.com>
2023-03-21 17:11:58 +0000

    Test Project Fixes

--- CodeHub.Tests/Controllers/HomeControllerTest.cs
+++ CodeHub.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using CodeHub.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CodeHub;
-using CodeHub.Controllers;
+using System.Web.Mvc;
 
 namespace CodeHub.Tests.Controllers
 {
